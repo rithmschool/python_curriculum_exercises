5908c42a25f0421193b7f9f847f43c615c70e910
Matt Lane <lane.matthew@gmail.com>
2017-08-15 15:25:44 -0700

    Update app.py

--- Unit-01/02-flask-routing/app.py
+++ Unit-01/02-flask-routing/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/add/<int:num1>/<int:num2>')
-def welcome(num1,num2):
+def add(num1,num2):
     total = num1 + num2
     return str(total)
 
